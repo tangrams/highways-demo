012eeeaddcf0f88f34e66b177c061066358f4967
Peter Richardson <github@zoomy.net>
2015-01-13 10:23:11 -0500

    incremental zoom

--- main.js
+++ main.js
@@ -14,7 +14,9 @@
 
     /*** Map ***/
 
-    var map = L.map('map');
+    var map = L.map('map',
+        {'keyboardZoomOffset': .05}
+    );
 
     var layer = Tangram.leafletLayer({
         source: {
