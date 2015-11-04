9fbb7bd8b11644e486e069fd8e56352008e62b24
Peter Richardson <github@zoomy.net>
2015-11-04 18:45:40 -0500

    trim vestigial functionality

--- main.js
+++ main.js
@@ -4,13 +4,7 @@
 map = (function () {
     'use strict';
 
-    var locations = {
-        'Oakland': [37.8044, -122.2708, 15],
-        'New York': [40.70531887544228, -74.00976419448853, 15],
-        'Seattle': [47.5937, -122.3215, 15]
-    };
-
-    var map_start_location = locations['Oakland'];
+    var map_start_location = [37.8044, -122.2708, 15]; // Oakland
 
     /*** URL parsing ***/
 
@@ -32,10 +26,7 @@ map = (function () {
 
     var layer = Tangram.leafletLayer({
         scene: 'scene.yaml',
-        numWorkers: 2,
-        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
-        unloadInvisibleTiles: false,
-        updateWhenIdle: false
+        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
     });
 
     window.layer = layer;
